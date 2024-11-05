b19800ea4bb6715a25f2eecb1e2d887d4093e5e7
Marc Rennefort <97467365+FlensBurger@users.noreply.github.com>
2024-11-05 14:15:49 +0100

    Add files via upload
    Aufgaben Überarbeitung - immernoch ohne aufgabe 3

---
+++ Homework 3/src/EntityManager.java
@@ -0,0 +1,30 @@
+import javax.swing.*;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.function.Predicate;
+
+public class EntityManager<T> {
+    private List<T> entities;
+
+    public EntityManager() {
+        this.entities = new ArrayList<T>();
+    }
+
+    public void addEntity(T entity) {
+        entities.add(entity);
+    }
+
+    public T getEntityByCriterion(Predicate criterion){
+        for(T entity : entities){
+            if(criterion.test(entity)){
+                return entity;
+            }
+        }
+        return null;
+    }
+
+    public List<T> getAllEntities() {
+        return entities;
+    }
+}
